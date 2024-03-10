c73744e933fd9e37267db868115220f97082f621
Franz Haimburger <frhaimburger@edu.aau.at>
2024-03-10 15:36:21 +0100

    Initialisierung der Ansichten und Beginn Codeteil für Serververbindung

--- app/src/main/java/com/example/se2einzelaufgabe/MainActivity.java
+++ app/src/main/java/com/example/se2einzelaufgabe/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.se2einzelaufgabe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String matrikelnummer;
+    private TextView inputMatrikelnummer;
+    private final String serverName = "se2-submission.aau.at";
+    private final int serverPort = 20080;
+    private Button button;
+    private TextView answerServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inputMatrikelnummer=findViewById(R.id.inputMatrikelnummer);
+        button=findViewById(R.id.button1);
+        answerServer=findViewById(R.id.textView4);
+
+        // Button-Klick-Ereignis
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectServer();
+            }
+        });
+
+
     }
+    private void connectServer () {
+
+        matrikelnummer=inputMatrikelnummer.getText().toString();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    //Verbindung zum Server herstellen
+                    Socket socket =  new Socket(serverName, serverPort);
+
+                    //Eingabe zum Server schicken
+                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(matrikelnummer);
+
+                    //Antwort vom Server empfangen
+                    BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String response = inputStream.readLine();
+
+                    out.close();
+                    socket.close();
+
+                    // Update UI on the main thread
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            answerServer.setText(response);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+        }).start();
+    }
+
 }
